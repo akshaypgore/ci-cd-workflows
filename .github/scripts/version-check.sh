4b4a0fcfb22f6c725412ac522f2fe9eed56d8c1d
akshaypgore <akshay.pradeep.gore@gmail.com>
2025-12-31 22:02:21 +0530

    Add check for snapshot keyword

--- .github/scripts/version-check.sh
+++ .github/scripts/version-check.sh
@@ -2,9 +2,15 @@
 
 version_set=$1
 current_prod_version=$2
+string_check="SNAP"
 echo "${version_set}"
 echo "${current_prod_version}"
 
+if [[ "$version_set" == *"$string_check"* ]]; then
+    echo "Found it!"
+else
+    echo " no Found it!"
+fi
 
 echo "Versions used ahead"
 echo "${version_set}"
