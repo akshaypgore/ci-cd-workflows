301df934f1e9b3f0578785f8ada418d985c0aa32
akshaypgore <akshay.pradeep.gore@gmail.com>
2025-12-31 21:54:19 +0530

    Add check for snapshot keyword

--- .github/scripts/version-check.sh
+++ .github/scripts/version-check.sh
@@ -2,16 +2,16 @@
 
 version_set=$1
 current_prod_version=$2
-# echo "${version_set}"
-# echo "${current_prod_version}"
+echo "${version_set}"
+echo "${current_prod_version}"
 
-# if [[ ${version_set} =~ "-SNAPSHOT" ]]; then
-#     echo "Please add snapshot"
-#     exit 1
-# else
-#     exit 0
-# fi
+if [[ "$version_set" =~ *"-SNAPSHOT"* ]]; then
+    echo "Please add snapshot"
+    exit 1
+else
+    exit 0
+fi
 
-# echo "Versions used ahead"
-# echo "${version_set}"
-# echo "${current_prod_version}"
+echo "Versions used ahead"
+echo "${version_set}"
+echo "${current_prod_version}"
