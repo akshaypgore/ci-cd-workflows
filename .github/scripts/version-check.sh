db0d7e314a54f4aa202af2f06a82e2a7d106d93b
akshaypgore <akshay.pradeep.gore@gmail.com>
2026-01-01 23:43:42 +0530

    Add more checks for version

--- .github/scripts/version-check.sh
+++ .github/scripts/version-check.sh
@@ -9,5 +9,13 @@ string_check="SNAPSHOT"
 if [[ "$version_set" == *"$string_check"* ]]; then
     echo "Version Deployed is $(echo $version_set | awk -F '-' '{print $2}')"
 else
-    echo " no Found it!"
+    echo "Please version the change as snapshot"
+    exit 1
+fi
+
+if [[ "printf '%s\n' '$(echo $version_set | awk -F "-" "{print $1}")' '$current_prod_version'|sort -V|head -n 1" == "$version_set" ]]; then
+    echo "Version set is less than or equal to version in prod. Please bump the version"
+    exit 1
+else
+    :
 fi
