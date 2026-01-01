29b68ef50a845f51c4bd1390242f5c874cd9cfba
akshaypgore <akshay.pradeep.gore@gmail.com>
2026-01-01 23:24:22 +0530

    Modify script to print version

--- .github/scripts/version-check.sh
+++ .github/scripts/version-check.sh
@@ -2,16 +2,12 @@
 
 version_set=$1
 current_prod_version=$2
-string_check="SNAP"
-echo "${version_set}"
-echo "${current_prod_version}"
+string_check="SNAPSHOT"
+# echo "${version_set}"
+# echo "${current_prod_version}"
 
 if [[ "$version_set" == *"$string_check"* ]]; then
-    echo "Found it!"
+    echo "Version Deployed is $(echo ${version_set}|awk -F "-" {print $2})"
 else
     echo " no Found it!"
 fi
-
-echo "Versions used ahead"
-echo "${version_set}"
-echo "${current_prod_version}"
